427e8fbbf11206c78f23adef3ecf2c0359593a31
matheusgabrielferraz0802@gmail.com <matheusgabrielferraz0802@gmail.com>
2022-04-24 13:11:04 -0300

    finalizand mais um Teste

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿int Número1 , Número2 , soma ;
+
+Console.Write("SOMA:\n");
+
+Console.Write("Digite o Primeiro Número:\n");
+Número1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Digite o Segundo Número:\n");
+Número2 = Convert.ToInt32(Console.ReadLine());
+
+soma = Número1 + Número2;
+
+
+Console.ForegroundColor = ConsoleColor.Blue;
+Console.WriteLine($"{soma}"!);
+Console.ResetColor();
